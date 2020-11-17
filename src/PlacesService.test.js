42fb2c5879cf5b97e437195269923e286031a39c
Mario Fink <mario.fink@goodyear.com>
2020-11-17 15:19:12 +0100

    mock autocomplete api function

--- src/PlacesService.test.js
+++ src/PlacesService.test.js
@@ -1,5 +1,27 @@
 import PlacesService from "./PlacesService";
 
-test("PlacesService throws when no service is passed in", () => {
-  expect(() => new PlacesService()).toThrow("NoServiceAvailable");
+const autocompleteMockOK = {
+  getPlacePredictions: (query, callback) => {
+    callback("success", "OK");
+  },
+};
+
+const autocompleteMockNotOK = {
+  getPlacePredictions: (query, callback) => {
+    callback("failure", "NOTOK");
+  },
+};
+
+describe("PlacesService", () => {
+  test("throws when no service is passed in", () => {
+    expect(() => new PlacesService()).toThrow("NoServiceAvailable");
+  });
+  test("Resolves when service is returning OK", () => {
+    const svc = new PlacesService(autocompleteMockOK);
+    expect(svc.autocomplete("myquery")).resolves.toEqual("success");
+  });
+  test("Rejects when service is not returning OK", () => {
+    const svc = new PlacesService(autocompleteMockNotOK);
+    expect(svc.autocomplete("myquery")).rejects.toEqual("NOTOK");
+  });
 });
