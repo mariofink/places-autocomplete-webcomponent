ba0f954e6f4c5209a9ee9e71aa1605179124802a
Mario Fink <mario.fink@goodyear.com>
2020-11-18 16:28:46 +0100

    add keyboard selection

--- src/places-autocomplete.js
+++ src/places-autocomplete.js
@@ -7,13 +7,15 @@ export class PlacesAutocomplete extends LitElement {
     return {
       label: { type: String },
       placeholder: { type: String },
-      suggestions: { type: Array },
+      suggestions: { type: Array, attribute: false },
+      selection: { type: Number, attribute: false },
     };
   }
 
   constructor() {
     super();
     this.suggestions = [];
+    this.selection = 0;
     this.placesService = new PlacesService(
       new google.maps.places.AutocompleteService()
     );
@@ -29,6 +31,9 @@ export class PlacesAutocomplete extends LitElement {
       label {
         display: block;
       }
+      .selected {
+        background: deeppink;
+      }
     `;
   }
 
@@ -39,17 +44,45 @@ export class PlacesAutocomplete extends LitElement {
         type="search"
         id="placesearch"
         placeholder=${this.placeholder}
-        @keyup=${this._onChange}
+        @keyup=${this._onKeyUp}
       />
       <ul>
-        ${this.suggestions.map((i) => html`<li>${i.description}</li>`)}
+        ${this.suggestions.map(
+          (suggestion, index) =>
+            html`<li class="${index === this.selection ? "selected" : ""}">
+              ${suggestion.description}
+            </li>`
+        )}
       </ul>
     `;
   }
 
-  _onChange(e) {
+  _onEnter() {
+    const event = new CustomEvent("place:change", {
+      detail: this.suggestions[this.selection],
+    });
+    this.dispatchEvent(event);
+  }
+
+  _onKeyUp(e) {
+    console.log("EVENT", e);
     this.value = e.target.value;
-    this.autocomplete();
+    switch (e.key) {
+      case "ArrowUp":
+        if (this.selection > 0) this.selection--;
+        break;
+
+      case "ArrowDown":
+        if (this.selection + 1 < this.suggestions.length) this.selection++;
+        break;
+
+      case "Enter":
+        this._onEnter();
+
+      default:
+        this.autocomplete();
+        break;
+    }
   }
 }
 
